2a737386beb742fa5f9ad561412de4ae2985c09e
Khojiakbar2000 <ishonkulovkhojiakbar@gmail.com>
2025-03-11 07:26:00 -0700

    fixed code

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,12 @@
-import dotenv from "dotenv";
-dotenv.config();
+console.log("Done")
 
-import mongoose from 'mongoose'
-import app from './app'
+import moment from 'moment'
+const currentTime = moment().format('YYYY MM DD')
+console.log(currentTime)
 
+const person: string = "Martin"
+const count: number = 100;
 
-mongoose.connect(process.env.MONGO_URL as string, {}).then(data =>{console.log('MongoDB connection succeed')
- const PORT = process.env.PORT ?? 3003;
- app.listen(PORT, function(){
-    console.log(`The server is running successfully on port: ${PORT}`)
- })
-}
-
-)
-.catch(err => console.log('ERROR on connection MongoDB', err))
 
 
 
