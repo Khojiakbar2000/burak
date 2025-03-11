3386c0d65ea908d30a2fa30d97e87671803f4c83
Khojiakbar2000 <ishonkulovkhojiakbar@gmail.com>
2025-03-10 23:34:47 -0700

    feat: integration of environment variables

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,6 @@
-console.log("executed")
+import dotenv from "dotenv";
+dotenv.config();
 
-import moment from 'moment'
-const currentTime = moment().format('YYYY MM DD')
-console.log(currentTime)
-
-const person: string = "Martin"
-const count: number = 100;
+console.log("PORT:", process.env.PORT);
+console.log("MONGO_URL:", process.env.MONGO_URL);
 
